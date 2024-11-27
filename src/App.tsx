03ecb1651e95f8cdffaeb1f077bdc3afde56f726
arnel360pixels <arnel@360pixels.co>
2024-11-27 20:54:03 +0800

    lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-
-  // console.log(first)
 
   return (
     <>
